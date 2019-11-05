212110adba947d5e6933c320e519cc956a512f08
Pibble.Fiasco <tiredorangecat@gmail.com>
2019-11-05 09:34:26 -0600

    Update generate_models.sh

--- word_data/_database/generate_models.sh
+++ word_data/_database/generate_models.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-DYLD_FALLBACK_LIBRARY_PATH=/Library/Postgresql/10/lib sqlacodegen postgresql://postgres:Bedlum!1@localhost/word_count
-
+DYLD_FALLBACK_LIBRARY_PATH=/Library/Postgresql/10/lib sqlacodegen 
